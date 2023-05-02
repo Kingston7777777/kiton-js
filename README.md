3e4a53b25a5c535c25d3c619a68d5fe22599ff53
Kingsley Anyichie <kingsleyanyichie9@gmail.com>
2023-05-02 10:33:04 -0700

    updated the framework

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# kiton-js
-I developed my first JavaScript framework
-Download the source code it has a text file that describe the kiton js module
+# kingsto-js
+
+## I developed my Javascript Modules called kiton js
+My Framework(module) kingsto js. It contains many functions and classes for web development.
+
+## Purpose of my Framework:
+1. To make it easier for developers who are not use to writing long code. So i wrote functions for javascript developers to make use of.
+2. It says time.
+3. I developed it not only for developers to make use of it but also for me to have experience developing frameworks/modules
+
+## What they module contain
+1. It contain a maths class that you can handle maths operation to know they operation like add() to add 2 numbers together, go to the file
+kiton.js i wrote comments that describe each class and functions. 
+
+## How to use it:
+### Step 1:
+If you want to use it in your nodejs project do
+const kiton = require("kingsto");
+then kingsto. to see all the classes and functions
+
+*Written by Your Faviourite programmer Which is Me Kingsley* 
--- kiton.js
+++ kiton.js
@@ -1,46 +1,76 @@
 //My Framework(module) kiton js It contains many functions and classes for web development
-export class MathFormulaFunction{//Maths class
-    static PI = 3.14159; 
-    static plus(x, y){//return 2 added values
-        return x + y;
+//Mostly Math class with loads of math functions to use in your web project
+const PI = 3.14159;
+class MathFormulaFunction{//Maths class 
+    static add(x, y){//return a value when adding x and y
+        return Number(x) + Number(y);
     }
-    static minus(x, y){//return 2 minus values
-        return x - y;
+    static minus(x, y){//return a value when substracting x and y
+        return Number(x) - Number(y);
     }
-    static div(x, y){//return 2 divided values
-        return x / y;
+    static div(x, y){//return a value when dividing x and y
+        return Number(x) / Number(y);
     }
-    static multiplication(x, y){//return 2 multiplied values
-        return x * y;
+    static multiplication(x, y){//return a value when multiplying x and y
+        return Number(x) * Number(y);
     }
-    static rememderOfDiv(x, y){//return 2 remender of division values
-        return x % y;
+    static rememderOfDiv(x, y){//return a value when modulos x and y to get the rememder of a division
+        return Number(x) % Number(y);
     }
-    static percentage(x, y){//return 2 remender of division values
-        return x / y * 100 +"%";
+    static percentage(x, y){//return a value when dividing x and y and multiplying it by 100
+        return Number(x) / Number(y) * 100;
     }
     static toCelsius(temp){//Convert to celsius
         temp = Number(temp);
-        return (temp - 32) * (5/9) +"°C";
+        return (temp - 32) * (5/9);
     }
     static toFahrenheit(temp){//Convert to fahrenheit
         temp = Number(temp);
-        return temp * 9 / 5 + 32 +"°F";
+        return temp * 9 / 5 + 32;
     }
-    static raiseTo2(x){//Mutiply a number by 2
-        return x * 2;
+    static raiseTo2(x){//Mutiply a number twice by itself e.g x * x
+        x = Number(x);
+        return x * x;
     }
-    static cube(x){//Mutiply a number by 3
-        return x * 3;
+    static cube(x){//Mutiply a number three times by itself e.g x * x * x
+        x = Number(x);
+        return x * x * x;
     }
-    static getCircumference(radius){
-        return 2 * MathFormulaFunction.PI * radius;
+    static getCircumference(radius){//getCircumference circle
+        return 2 * PI * Number(radius);
     }
-    static getArea(radius){
-        return MathFormulaFunction.PI * radius * radius;
+    static getArea(radius){//to getArea of a circle
+        return PI * Number(radius) * Number(radius);
+    }
+    static highestValueNumber(x, y){//To get the highest value between 2 numbers
+        x = Number(x);
+        y = Number(y);
+        if(x > y){
+            return x;
+        }
+        else if(y > x){
+            return y;
+        }
+        else{
+            return;
+        }
+    }
+    static lowestValueNumber(x, y){//To get the lowest value between 2 numbers
+        x = Number(x);
+        y = Number(y);
+        if(x < y){
+            return x;
+        }
+        else if(y < x){
+            return y;
+        }
+        else{
+            return;
+        }
     }
 }
-export class Arrays{//Arrays class
+
+class Arrays{//Arrays class
     static printElementOfArrayFS(array){//To print elements in an array from the beginning using for loop
         for(let i = 0; i < array.length; i+=1){
             console.log(array[i]);
@@ -51,16 +81,16 @@ export class Arrays{//Arrays class
             console.log(array[i]);
         }
     }
-    static print2DArray(array){//put the array that holds multiple array
-        for(let twoList of array){
-            for(let eachListInTwoD of twoList){
-                console.log(eachListInTwoD);
+    static print2DArray(array){//print 2D array
+        for(let i of array){
+            for(let j of i){
+                console.log(j);
             }
         }    
     }
-    static reduceArrayToASingleElement(array){//reduce an array to a single element
+    static reduceArrayToASingleElement(array){//reduce an array to a single element. REMEMBER a single array must be pass not 2D
         function check(total, element){
-        return total + element;//The total is the first element + the rest of the elements in an array
+        return total + element;//Add up the element together
         }
         return array.reduce(check);
     }
@@ -80,20 +110,5 @@ export class Arrays{//Arrays class
     }
     
 }
-export function printToConsole(x){//Function to print out an ouput on the console
-    console.log(x);
-}
-export function open(address){//Function to open a window put an address and it will open a window
-    window.open(address);
-}
-export function logic(x){//Function to check if something is true or false
-    if(x){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
 
-
-
+module.exports = {MathFormulaFunction, Arrays};
